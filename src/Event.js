bfe592b4f0a0c19e7c1fe9d23d6f9926a8450f16
ponCHANNN <mizuki.7302@gmail.com>
2023-01-11 15:41:57 +0900

    add calendar

--- src/Event.js
+++ src/Event.js
@@ -1,28 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from '@rneui/base';
-
+import { Calendar } from 'react-native-calendars';
+//yarn add react-native-calendars
 export default function Event() {
   return (
-    <View style={styles.container}>
-      <Text>これはイベントカレンダー画面です</Text>
-      <Button
-        title="Click Me"
-        onPress={()=>{
-          alert("アラート表示");
-          console.log("コンソールログ")
-        }}
+      <Calendar 
+      enableSwipeMonths
+      style={{height:"100%"}}
+      theme = {{
+        'stylesheet.calendar.main': {
+          monthView: {
+            flex: 1,
+            height: '100%',
+            justifyContent: 'space-around'
+          },
+          week: {
+            flex: 1,
+            marginVertical: 0,
+            flexDirection: 'row',
+            justifyContent: 'space-around'
+          },
+          dayContainer: {
+            borderColor: '#f5f5f5',
+            borderWidth: 1,
+            flex:1,
+          },
+        }
+      }}
+      onDayPress = {day => {
+        console.log(day);
+      }}
       />
-      <StatusBar style="auto" />
-    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
