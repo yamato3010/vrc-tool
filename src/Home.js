ce85d866e4bc017d84c0f92aac1b2483a344ad76
DESKTOP-SD0H0H3\yama3 <yama3to10yama3@gmail.com>
2023-01-14 15:56:13 +0900

    フレンド一覧を取得可能に

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
 import { Button } from '@rneui/base';
+import axios from 'axios';
 
 export default function Home(props) {
-  return (
-    <View style={styles.container}>
-      <Text>フレンド一覧を表示するためにはVRChatアカウントでログインする必要があります</Text>
-      <Button
-        title="ログイン"
-        onPress={()=>{
-          //ログイン画面へ
-          props.navigation.navigate('Login')
-        }}
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [ok, setOk] = useState(null);
+  const [friends, setFriends] = useState(null);
+
+  global.instance = axios.create({
+    withCredentials: true,
+    baseURL: "https://api.vrchat.cloud/api/1"
+    });
+
+  useEffect(() => {
+    console.log("現在のセッションが有効か確認")
+    instance
+      .get('https://api.vrchat.cloud/api/1/auth', {
+        withCredentials: true
+      })
+      .then(res => {
+        console.log("成功");
+        console.log(res.data.ok);
+        if (res.data.ok == true){
+          setOk(true);
+          //フレンド一覧を取得する
+          instance
+          .get('https://api.vrchat.cloud/api/1/auth/user/friends?offline=true', {
+            withCredentials: true
+          })
+          .then(res => {
+            console.log("フレンド一覧を取得しました");
+            console.log(res.data);
+            setFriends(res.data);
+          })
+          .catch(err => {
+            console.log("フレンド一覧の取得に失敗しました");
+            console.log(err.response);
+          })
+        } //セッションが有向であるときにtrueを返す
+        else { //セッションが無効な時にfalseを返す
+          console.log("セッションが無効です");
+          setOk(false);
+        }
+      })
+      .catch(err => {
+        console.log("セッションの確認ができませんでした");
+        console.log(err);
+        setOk(false);
+      })
+  }, []);
+  if(ok == null) return null;
+  if(ok == true){ // セッションが有効な場合，フレンド一覧を出す
+    return (
+      <View style={styles.container}>
+        <Text>有効なセッションです。ログインは不要です。</Text>
+        <Text>フレンド一覧</Text>
+        {friends.map((friend, i) => <Text key={i}>{i}.{friend.displayName}</Text>)}
+        <StatusBar style="auto" />
+      </View>
+    );
+  }else{ // セッションが無効な場合，ログインを促す
+    return (
+      <View style={styles.container}>
+        <Text>フレンド一覧を表示するためにはVRChatアカウントでログインする必要があります</Text>
+        <Button
+          title="ログイン"
+          onPress={()=>{
+            //ログイン画面へ
+            props.navigation.navigate('Login')
+          }}
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- src/Login.js
+++ src/Login.js
@@ -3,42 +3,30 @@ import { StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
 import { Input } from '@rneui/themed';
 import { Button } from '@rneui/base';
-import axios from 'axios';
 
 export default function Login(state) {
   const [username, setUsername] = useState(null); //テキストフィールドに入力されたユーザIDが入る
   const [password, setPassword] = useState(null); //テキストフィールドに入力されたパスワードが入る
 
-  global.instance = axios.create({
-    withCredentials: true,
-    baseURL: "https://api.vrchat.cloud/api/1"
-    });
-
   const login = async (userid, password) => {
-    let ok = await verify();
-    if (ok == true) { //TODO:ここが機能していない。verify関数からtrue or falseが返ってきて来てから処理を行いたい。
-      console.log("cookieあるよ");
-      console.log(global.cookie[0]);
-    }
-    else {
-      console.log("cookieがないので取得")
-      // instance
-      //   .get('https://api.vrchat.cloud/api/1/auth/user', {
-      //     headers: {
-      //       'Authorization': { username: userid, password: password },
-      //     },
-      //     withCredentials: true
-      //   })
-      //   .then(res => {
-      //     console.log("成功");
-      //     console.log(res);
-      //     global.cookie = res.headers['set-cookie'];
-      //   })
-      //   .catch(err => {
-      //     console.log("error");
-      //     console.log(err);
-      //   })
-    }
+      console.log("ログインします")
+      instance
+        .get('https://api.vrchat.cloud/api/1/auth/user', {
+          auth: {
+            username: userid,
+            password: password,
+          },
+          withCredentials: true
+        })
+        .then(res => {
+          console.log("成功");
+          console.log(res);
+          // global.cookie = res.headers['set-cookie'];
+        })
+        .catch(err => {
+          console.log("error");
+          console.log(err.response);
+        })
   }
 
   const verify = async () => {
@@ -59,22 +47,6 @@ export default function Login(state) {
         })
   }
 
-  const testCookie = () => { //テスト用関数。将来的に削除
-    console.log("cookieを使って情報を取得します");
-    instance
-        .get('https://api.vrchat.cloud/api/1/auth/user/friends', {
-          
-          withCredentials: true
-        })
-        .then(res => {
-          console.log("成功");
-          console.log(res);
-        })
-        .catch(err => {
-          console.log("error");
-          console.log(err);
-        })
-  }
   return (
     <View style={styles.container}>
       <Text>VRChat ID(ユーザーネーム)とパスワードを入力してください</Text>
@@ -101,24 +73,14 @@ export default function Login(state) {
       <Button
         title="ログインテスト"
         onPress={async () => {
+          login("testAvater", "ponchannnyamato");
         }}
       />
       <Button
         title="cookieテスト"
         onPress={async () => {
-          // await verify()
-          // .then(() => {
-          //   if(ok){
-          //     console.log("セッション有効");
-          //   }else{
-          //     console.log("無効なセッション");
-          //   }
-          // })
-          // .catch((res) => {
-          //   console.log(res);
-          //   console.log("認証の確認時に何らかのエラー");
-          // })
-          console.log(verify()); //TODO:verify()から結果が返されてから処理を行うようにする
+          let res = await verify();
+          console.log(res); //TODO:verify()から結果が返されてから処理を行うようにする
         }}
       />
       <StatusBar style="auto" />
