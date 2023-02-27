0044ef64da5343466550f7bc7b4cff08e5efe9f1
yamato3010 <yama3to10yama3@gmail.com>
2023-02-28 03:30:18 +0900

    少しレイアウトを変更

--- src/UserInfo.tsx
+++ src/UserInfo.tsx
@@ -15,16 +15,17 @@ export default function UserInfo({ route }) {
                     <Card.Cover source={{ uri: route.params.data.currentAvatarImageUrl }} />
                     <Card.Content>
                         <Text variant="titleLarge">{ route.params.data.displayName } </Text>
-                        <Text>Status: { route.params.data.status }
-                        { route.params.data.status=="active" ? <Text style={{color: 'green'}}>●</Text>
-                        : route.params.data.status=="join me" ? <Text style={{color: 'skyblue'}}>●</Text>
-                        : route.params.data.status=="ask me" ? <Text style={{color: 'orange'}}>●</Text>
-                        : route.params.data.status=="busy" ? <Text style={{color: 'red'}}>●</Text>
-                        : <Text style={{color: 'gray'}}>●</Text>
-                    }
+                        <Text variant="bodyLarge">
+                            { 
+                                route.params.data.status=="active" ? <Text style={{color: 'green'}}>●</Text>
+                                : route.params.data.status=="join me" ? <Text style={{color: 'skyblue'}}>●</Text>
+                                : route.params.data.status=="ask me" ? <Text style={{color: 'orange'}}>●</Text>
+                                : route.params.data.status=="busy" ? <Text style={{color: 'red'}}>●</Text>
+                                : <Text style={{color: 'gray'}}>●</Text>
+                            }
+                            { route.params.data.status }
+                            {"\n"}
                         </Text>
-                        
-                        <Text variant="bodyMedium">Bio:</Text>
                         <Text variant="bodyMedium">{ route.params.data.bio }</Text>
                     </Card.Content>
                     <Card.Actions>
