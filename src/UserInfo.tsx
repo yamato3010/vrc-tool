25f14578ab0c63fcdce8d3ba071baae119aa7c87
yamato3010 <yama3to10yama3@gmail.com>
2023-02-28 03:09:41 +0900

    とりあえず雑に表示できるように

--- src/UserInfo.tsx
+++ src/UserInfo.tsx
@@ -1,19 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Card, Text } from 'react-native-paper';
 
-export default function UserInfo({route}) {
+export default function UserInfo({ route }) {
     return (
-        <View style={styles.container}>
-            <Text>{route.params.data.displayName}</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.container}>
+                <Card style={{
+                        backgroundColor: 'white',
+                        borderColor: 'black',
+                        marginLeft: 1,
+                        marginRight: 1,
+                    }}>
+                    <Card.Cover source={{ uri: route.params.data.currentAvatarImageUrl }} />
+                    <Card.Content>
+                        <Text variant="titleLarge">{ route.params.data.displayName } </Text>
+                        <Text>Status: { route.params.data.status }
+                        { route.params.data.status=="active" ? <Text style={{color: 'green'}}>●</Text>
+                        : route.params.data.status=="join me" ? <Text style={{color: 'skyblue'}}>●</Text>
+                        : route.params.data.status=="ask me" ? <Text style={{color: 'orange'}}>●</Text>
+                        : route.params.data.status=="busy" ? <Text style={{color: 'red'}}>●</Text>
+                        : <Text style={{color: 'gray'}}>●</Text>
+                    }
+                        </Text>
+                        
+                        <Text variant="bodyMedium">Bio:</Text>
+                        <Text variant="bodyMedium">{ route.params.data.bio }</Text>
+                    </Card.Content>
+                    <Card.Actions>
+                    </Card.Actions>
+                </Card>
+            </View>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      alignItems: 'stretch',
-      justifyContent: 'center',
+        flex: 1,
+        alignItems: 'stretch',
+        justifyContent: 'center',
     }
-  });
-  
+});
