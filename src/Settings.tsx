3452720a13ca155548dd44a18745b8c82e7e90e9
yamato3010 <yama3to10yama3@gmail.com>
2023-03-02 00:58:32 +0900

    とりあえず設定画面を追加

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,28 +1,36 @@
+import { Entypo, Feather } from '@expo/vector-icons';
+import { ListItem } from '@rneui/themed';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from '@rneui/base';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { version } from '../package.json';
 
 export default function Settings(props) {
   return (
-    <View style={styles.container}>
-      <Text>これは設定画面です</Text>
-      <Button
-        title="イベントカレンダーへ"
-        onPress={()=>{
-          console.log("コンソールログ")
-          props.navigation.navigate('Event')
-        }}
-      />
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <ListItem>
+        <Entypo name="log-out" size={24} color="#3478F7" />
+          <ListItem.Content>
+            <ListItem.Title>ログアウト</ListItem.Title>
+          </ListItem.Content>
+          <ListItem.Chevron />
+        </ListItem>
+        <ListItem>
+        <Feather name="info" size={24} color="#3478F7" />
+          <ListItem.Content>
+            <ListItem.Title>バージョン{version}</ListItem.Title>
+          </ListItem.Content>
+        </ListItem>
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
   },
 });
